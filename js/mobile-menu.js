bd8d9e35a05de88b4ecdd2289f10ee914195e413
Emil Backlund <Emil.backlund@stud.noroff.no>
2022-05-10 11:37:28 +0200

    Starting on contact page

---
+++ js/mobile-menu.js
@@ -0,0 +1,10 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const ShowMobileMenu = () => {
+  hamburgerMenu.classList.toggle("is-active");
+  hamburgerMenu.classList.toggle("active--hamburger-icon");
+  mobileMenu.classList.toggle("active-mobile-menu");
+};
+
+hamburgerMenu.addEventListener("click", ShowMobileMenu);
